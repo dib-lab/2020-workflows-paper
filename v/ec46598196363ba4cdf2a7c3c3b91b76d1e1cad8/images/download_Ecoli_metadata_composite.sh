421633b5652d30d6e41681a115c1409d2a0a3f3c
Tessa Pierce <bluegenes@users.noreply.github.com>
2020-05-30 19:35:54 +0000

    minor word changes [ci skip]
    This build is based on
    https://github.com/dib-lab/2020-workflows-paper/commit/ec46598196363ba4cdf2a7c3c3b91b76d1e1cad8.
    
    This commit was created by the following CI build and job:
    https://github.com/dib-lab/2020-workflows-paper/commit/ec46598196363ba4cdf2a7c3c3b91b76d1e1cad8/checks
    https://github.com/dib-lab/2020-workflows-paper/runs/120086750

---
+++ v/ec46598196363ba4cdf2a7c3c3b91b76d1e1cad8/images/download_Ecoli_metadata_composite.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# this script downloads the Ecoli metadata composite from the 
+# data carpentry genomics wrangling genomics repository.
+wget https://raw.githubusercontent.com/datacarpentry/wrangling-genomics/gh-pages/files/Ecoli_metadata_composite.tsv
